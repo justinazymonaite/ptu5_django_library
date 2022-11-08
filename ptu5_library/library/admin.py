281193cf05c731ab135a12ae857417a3f78a57bf
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-08 11:43:05 +0200

    search fields, list editable, list display

--- ptu5_library/library/admin.py
+++ ptu5_library/library/admin.py
@@ -18,14 +18,19 @@ class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('unique_id', 'book', 'status', 'due_back')
     list_filter = ('status', 'due_back')
     readonly_fields = ('unique_id', ) # butinai reikia kablelio, kad butu kaip tuple
+    search_fields = ('unique_id', 'book__title', 'book__author__last_name__exact') # django lookups
+    list_editable = ('status', 'due_back') # nusirodom laukus, kuriuos norim redaguoti vietoje
 
     fieldsets = (
         ('General', {'fields': ('unique_id', 'book')}),
         ('Availability', {'fields': (('status', 'due_back'),)}), # idejus tuple i tuple galime padaryti, kad laukai atsidurtu vienoje eiluteje
     )
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'display_books')
+    list_display_links = ('last_name', )
 
-admin.site.register(models.Author)
+admin.site.register(models.Author, AuthorAdmin)
 admin.site.register(models.Genre)
 admin.site.register(models.Book, BookAdmin)
 admin.site.register(models.BookInstance, BookInstanceAdmin)
