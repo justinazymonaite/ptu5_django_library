76ef3ed176e08337739176232e5ed6ad1eb45e30
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-17 11:47:03 +0200

    django 9 paskaita user registration tesinys

--- ptu5_library/user_profile/views.py
+++ ptu5_library/user_profile/views.py
@@ -13,17 +13,26 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
+        error = False
         if not username or User.objects.filter(username=username).first():
             messages.error(request, 'Username not entered or username already exists.')
+            error = True
         if not email or User.objects.filter(email=email).first():
             messages.error(request, 'Email not entered or user with this email already exists.')
+            error = True
         else:
             try:
                 validate_email(email)
             except:
                 messages.error(request, 'Invalid email.')
+                error = True
         if not password or not password2 or password != password2:
             messages.error(request, 'Passwords not entered or do not match.')
+            error = True
+        if not error:
+            new_user = User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, f'User {username} registration successful. You can log in now.')
+            return redirect('login')
 
     return render(request, 'user_profile/register.html')
 
